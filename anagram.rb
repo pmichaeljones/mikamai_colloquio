d3d90844a708030aa7ab1309c90846e9609151bf
Patrick Jones <pmichaeljones@gmail.com>
2017-11-02 13:30:38 +0100

    clean commits for the feature branch
    second commit on feature branch
    
    a third thing
    
    even more extra stuff

--- anagram.rb
+++ anagram.rb
@@ -1,35 +1,8 @@
-class AnagramSolver
+here I add something.
 
-  require 'open-uri'
-  require 'pry'
+then I write another thing here. This is the second new commit on the new feature branch. 
 
-  attr_reader :words_array
+thenwe have a thing thing here.
 
-  def initialize
-    @words_array = []
-    @sorted_words = []
-  end
 
-  def load_words
-    words = open("http://codekata.com/data/wordlist.txt").read
-    words.each_line do |word|
-      @words_array << word.chomp.downcase.gsub(/'.*/, "")
-    end
-    @words_array.each do |word|
-      @sorted_words << word.chars.sort.join
-    end
-  end
-
-  def find_anagram(word)
-    if @sorted_words.include?(word.gsub(/ /,'').chars.sort.join)
-      puts word
-    end
-  end
-
-end
-
-solver = AnagramSolver.new
-
-solver.load_words
-
-binding.pry
+more stuff here
